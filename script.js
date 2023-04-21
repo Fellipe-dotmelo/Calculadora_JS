7636f4700dd43d792f737643306f0d74b51c50e2
Fellipe-dotmelo <61851972+Fellipe-dotmelo@users.noreply.github.com>
2023-04-21 16:41:52 -0300

    atualizei os erros de lógica e mudei as cores da calculadora, agora você só pode usar um operador númerico por vez

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 function calcular(tipo, valor) {
-  if(tipo === 'acao'){
-    if(valor === 'c'){
-    //limpar conteúdo do input
-    document.getElementById('resultado').value = ''
+  if (tipo === 'acao') {
+    if (valor === 'c') {
+      //limpar conteúdo do input
+      document.getElementById('resultado').value = ''
+    } else if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+      //verificar se o último caractere é um operador
+      var ultimoCaractere = document.getElementById('resultado').value.slice(-1)
+      if (ultimoCaractere === '+' || ultimoCaractere === '-' || ultimoCaractere === '*' || ultimoCaractere === '/' || ultimoCaractere === '.') {
+        //substituir o último operador pelo novo operador
+        document.getElementById('resultado').value = document.getElementById('resultado').value.slice(0, -1) + valor
+      } else {
+        //adicionar o novo operador ao valor do input
+        document.getElementById('resultado').value += valor
+      }
+    } else if (valor === '=') {
+      var valor_campo = eval(document.getElementById('resultado').value)
+      document.getElementById('resultado').value = valor_campo
     }
-      if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
-      document.getElementById('resultado').value += valor
-      }
-        if(valor === '='){
-        var valor_campo = eval(document.getElementById('resultado').value)
-        document.getElementById('resultado').value = valor_campo
-      }
-  }
-  else if(tipo === 'valor'){
+  } else if (tipo === 'valor') {
     document.getElementById('resultado').value += valor
   }
 }
